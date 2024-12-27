ac4b087952cd4a187ef58875964be2ec84934185
NikhilPrajith <57269214+NikhilPrajith@users.noreply.github.com>
2024-12-27 17:26:27 -0500

    Fix

--- settings/navigation.tsx
+++ settings/navigation.tsx
@@ -4,17 +4,21 @@ export const Navigations = [
   {
     title: "Home",
     href: "/",
+    external: false
   },
   {
     title: "Dashboard",
     href: "/dashboard",
+    external: false
   },
   {
     title: "Explore",
     href: "/explore",
+    external: false
   },
   {
     title: "Buy Me A Coffee",
     href: "/support",
+    external: false
   },
 ];
